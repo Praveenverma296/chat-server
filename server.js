fa447cb8e1a5762994f80d76d89705ff12fe7e3c
Praveenverma296 <pv08429@gmail.com>
2026-04-30 16:37:37 +0530

    chat server project

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  // Join Room
+  socket.on("join_room", (room) => {
+    socket.join(room);
+    console.log(`User joined room: ${room}`);
+  });
+
+  // Send Message
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(5001, () => {
+  console.log("Chat server running on port 5001");
+});
